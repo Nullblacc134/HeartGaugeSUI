361064834dcf3d9ee634939e669c029a37f3435a
Kenny <kennymaylla@gmail.com>
2025-01-15 14:13:36 -0500

    Initial Commit

---
+++ HeartGaugeSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartGaugeSUI
+//
+//  Created by Kenny Mayancela Aylla on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartGaugeSUI/HeartGaugeSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartGaugeSUIApp.swift
+//  HeartGaugeSUI
+//
+//  Created by Kenny Mayancela Aylla on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartGaugeSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
