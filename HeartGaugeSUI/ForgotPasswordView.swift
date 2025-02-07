1401c7126e65b94a287425cdb96b60b237636c28
Kenny <kennymaylla@gmail.com>
2025-02-07 13:52:30 -0500

    heroo

---
+++ HeartGaugeSUI/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  HeartGaugeSUI
+//
+//  Created by Kenny Mayancela Aylla on 2/4/25.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
