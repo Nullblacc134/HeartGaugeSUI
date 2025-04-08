e66866b6ec4cb15737221395087a41855b562008
Kenny <kennymaylla@gmail.com>
2025-04-08 13:35:16 -0400

    jn

---
+++ LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  HeartGaugeSUI
+//
+//  Created by Kenny Mayancela Aylla on 4/3/25.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
